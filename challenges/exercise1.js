fb182ce09f0ae7e3cf6ea6b17f241e9f821faf3a
rizwan gulamhussein <rizwan.g@live.com>
2023-10-11 09:28:08 +0100

    fix :Formatted document

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,54 +1,51 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-  else{
+  if (sandwich === undefined) throw new Error('ingredients is required');
+  else {
     return sandwich.fillings;
   }
 
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	return (person.city === 'Manchester');
+  if (person === undefined) throw new Error('person is required');
+  return (person.city === 'Manchester');
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	if ((people >= 1) && (people <= 40))
-  {
+  if (people === undefined) throw new Error('people is required');
+  if ((people >= 1) && (people <= 40)) {
     return 1;
   }
-  else if ((people >= 41) && (people <= 80))
-  {
+  else if ((people >= 41) && (people <= 80)) {
     return 2;
   }
-  else if ((people >= 81) && (people <= 120))
-  {
+  else if ((people >= 81) && (people <= 120)) {
     return 3;
   }
-  else{
-    if ((people % 40) === 0){
-      return Math.floor(people/40);
+  else {
+    if ((people % 40) === 0) {
+      return Math.floor(people / 40);
     }
-    else{
-      return Math.floor(people/40) + 1;
+    else {
+      return Math.floor(people / 40) + 1;
     }
- }
+  }
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
+  if (arr === undefined) throw new Error('arr is required');
   let count = 0;
   arr.forEach((animalType) => {
-    if (animalType === 'sheep'){
+    if (animalType === 'sheep') {
       count++;
     }
-});
-return count;
+  });
+  return count;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-  
-	return ((person.address.postCode.charAt(0) === 'M')
-  && (person.address.city === 'Manchester'))
+  if (person === undefined) throw new Error('person is required');
+
+  return ((person.address.postCode.charAt(0) === 'M')
+    && (person.address.city === 'Manchester'))
 }
