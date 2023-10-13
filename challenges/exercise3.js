1202f1928d50bc12e7cd60722df5254c6ea19353
rizwan gulamhussein <rizwan.g@live.com>
2023-10-13 19:27:55 +0100

    feat: Implemented exercise three using forLoop - getSquareRoots

--- challenges/exercise3.js
+++ challenges/exercise3.js
@@ -1,7 +1,7 @@
 export function findSmallNums(nums) {
-  if (!nums) throw new Error('nums is required');
+  if (!nums) throw new Error("nums is required");
   let arraySmallNums = [];
-  nums.forEach(element => {
+  nums.forEach((element) => {
     if (element < 1) {
       arraySmallNums.push(element);
     }
@@ -10,51 +10,54 @@ export function findSmallNums(nums) {
 }
 
 export function findNamesBeginningWith(names, char) {
-  if (!names) throw new Error('names is required');
-  if (!char) throw new Error('char is required');
+  if (!names) throw new Error("names is required");
+  if (!char) throw new Error("char is required");
   return names.filter((word) => word.charAt(0) === char);
 }
 
 export function findVerbs(words) {
-  if (!words) throw new Error('words is required');
-  return words.filter((word) => (word.charAt(0) === 't') &&
-    (word.charAt(1) === 'o') && (word.charAt(2) === ' '))
+  if (!words) throw new Error("words is required");
+  return words.filter(
+    (word) =>
+      word.charAt(0) === "t" && word.charAt(1) === "o" && word.charAt(2) === " "
+  );
 }
 
 export function getIntegers(nums) {
-  if (!nums) throw new Error('nums is required');
+  if (!nums) throw new Error("nums is required");
   const result = [];
   console.log(nums);
   nums.map((item) => {
-    if (Number.isInteger(item))
-      result.push(item);})
+    if (Number.isInteger(item)) result.push(item);
+  });
   return result;
-
 }
 
 export function getCities(users) {
-  if (!users) throw new Error('users is required');
+  if (!users) throw new Error("users is required");
   let arrayOfCityFromUser = [];
-  users.forEach(user => {
-      console.log(user.data.city.displayName);
-      arrayOfCityFromUser.push(user.data.city.displayName);
-
+  users.forEach((user) => {
+    console.log(user.data.city.displayName);
+    arrayOfCityFromUser.push(user.data.city.displayName);
   });
   return arrayOfCityFromUser;
 }
 
 export function getSquareRoots(nums) {
-  if (!nums) throw new Error('nums is required');
-  // Your code here
+  if (!nums) throw new Error("nums is required");
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = Math.round(Math.sqrt(nums[i]) * 100) / 100;
+  }
+  return nums;
 }
 
 export function findSentencesContaining(sentences, str) {
-  if (!sentences) throw new Error('sentences is required');
-  if (!str) throw new Error('str is required');
+  if (!sentences) throw new Error("sentences is required");
+  if (!str) throw new Error("str is required");
   // Your code here
 }
 
 export function getLongestSides(triangles) {
-  if (!triangles) throw new Error('triangles is required');
+  if (!triangles) throw new Error("triangles is required");
   // Your code here
 }
