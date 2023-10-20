b222961113b96c882c943c935f06a9df30bbbf2b
rizwan gulamhussein <rizwan.g@live.com>
2023-10-20 12:30:40 +0100

    feat Implemented exercise four - getWordFrequencies

--- challenges/exercise4.js
+++ challenges/exercise4.js
@@ -57,10 +57,45 @@ export const arrShift = (arr) => {
 export const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
-};
+  const asArray = Object.entries(haystack);
+  console.log(asArray);
 
+  console.log(asArray.length);
+  for (let arrayIndex = 0; arrayIndex < asArray.length; arrayIndex++) {
+    console.log(arrayIndex);
+    console.log(asArray[arrayIndex].length);
+    for (
+      let subArrayIndex = 0;
+      subArrayIndex < asArray[arrayIndex].length;
+      subArrayIndex++
+    ) {
+      console.log(asArray[arrayIndex][subArrayIndex]);
+      if (asArray[arrayIndex][subArrayIndex] === searchTerm) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
 export const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  //   let format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+
+  str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+
+  let wordArray = str.split(" ");
+  let wordCounter = {};
+
+  for (let i = 0; i < wordArray.length; i++) {
+    let newWord = wordArray[i].toLowerCase();
+    if (wordCounter[newWord]) {
+      wordCounter[newWord] += 1;
+    } else {
+      wordCounter[newWord] = 1;
+    }
+  }
+
+  console.log(wordCounter);
+  return wordCounter;
 };
